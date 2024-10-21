aec0e21eab52bcdb4a8036363bb51524110ef94a
Wahandri <wahandrimasuno@gmail.com>
2024-10-21 06:00:49 +0200

    Change in fetch routes

--- backEnd/server.js
+++ backEnd/server.js
@@ -50,6 +50,6 @@ const SEED =
   "4b343153cf67b908b5b57cf85d2e06c33cd846efa04299ecb88a77fb6af652098651c83079ad8ec68657cb15a6e34f532fcee4b1e067993e55252660ffe60907";
 
 // Escuchando en el puerto
-app.listen(port, () => {
+app.listen(3001, "0.0.0.0", () => {
   console.log('Conexion con el puerto:', port);
 });
