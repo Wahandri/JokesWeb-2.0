69f1ae1cd2613afb8540c08042a054ecdaa0de74
Wahandri <wahandricode@gmail.com>
2024-10-27 23:55:55 +0100

    Update Config of URL

--- frontEnd/src/components/configURL.js
+++ frontEnd/src/components/configURL.js
@@ -1,6 +1,6 @@
 // configURL.js
 const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'https://54.38.184.19:3001'  // URL en producción
+  ? 'https://wahandri.es'  // URL en producción
   : 'http://localhost:3001'; // URL en desarrollo
 
 export default apiUrl;
