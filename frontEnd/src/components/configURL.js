5011a5f43447ec3a55cb2a182ed8ff6b7b63e285
Wahandri <wahandrimasuno@gmail.com>
2024-11-20 07:31:10 +0100

    update configUrl

--- frontEnd/src/components/configURL.js
+++ frontEnd/src/components/configURL.js
@@ -1,6 +1,6 @@
 // configURL.js
 const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'http://217.160.2.20'  // URL en producción
+  ? 'http://217.160.2.20:5000'  // URL en producción
   : 'http://localhost:3001'; // URL en desarrollo
 
 export default apiUrl;
