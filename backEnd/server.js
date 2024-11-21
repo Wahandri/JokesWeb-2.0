509eea644fae2887e52f7d651cd4b6e88c76072b
Wahandri <wahandrimasuno@gmail.com>
2024-11-21 01:21:23 +0100

    update certificate

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv"); 
 dotenv.config();
 
+const fs = require('fs');
+const https = require('https');
 const users = require("./routes/users");
 const jokes = require("./routes/jokes");
 const login = require("./routes/login");
@@ -14,15 +16,15 @@ const bodyParser = require("body-parser");
 
 // Usando middlewares para leer JSON y habilitar CORS
 app.use(bodyParser.json());
-//  app.use(cors());
 
 app.use(cors({
-    origin: ["https://jokes-web.vercel.app", "https://jokes-web-2-0.vercel.app"], // Origenes permitidos
+    origin: ["https://jokes-web.vercel.app", "https://jokes-web-2-0.vercel.app"], // Orígenes permitidos
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Permite todos los métodos
     allowedHeaders: ["Content-Type", "Authorization"], // Permite encabezados específicos
     credentials: true // Si necesitas cookies o encabezados de autorización
 }));
 
+// Rutas
 app.use("/users", users);
 app.use("/jokes", jokes);
 app.use("/login", login);
@@ -49,7 +51,22 @@ const SEED =
   process.env.SEED ||
   "4b343153cf67b908b5b57cf85d2e06c33cd846efa04299ecb88a77fb6af652098651c83079ad8ec68657cb15a6e34f532fcee4b1e067993e55252660ffe60907";
 
-// Escuchando en el puerto
-app.listen(3001, "0.0.0.0", () => {
-  console.log('Conexion con el puerto:', port);
+// Leer certificados SSL
+const privateKey = fs.readFileSync('/etc/ssl/private/private.key', 'utf8');
+const certificate = fs.readFileSync('/etc/ssl/certs/certificate.crt', 'utf8');
+const ca = certificate; // Los certificados autofirmados no necesitan CA
+
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+// Crear el servidor HTTPS
+const httpsServer = https.createServer(credentials, app);
+
+// Iniciar el servidor HTTPS
+httpsServer.listen(3001, () => {
+    console.log('Servidor HTTPS corriendo en el puerto 3001');
 });
--- frontEnd/src/components/configURL.js
+++ frontEnd/src/components/configURL.js
@@ -1,6 +1,6 @@
 // configURL.js
 const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'http://217.160.2.20:3001'  // URL en producción
+  ? 'https://217.160.2.20:3001'  // URL en producción
   : 'http://localhost:3001'; // URL en desarrollo
 
 export default apiUrl;
